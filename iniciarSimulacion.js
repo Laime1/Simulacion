03b400ca23c92274c5d32a84080e66445a0225ec
Laime Antonio <anlaifel09@gmail.com>
2023-11-06 09:00:32 -0400

    ordenando

--- iniciarSimulacion.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("simulacion-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    // Abre una nueva página después de iniciar la simulación
-    window.open("mostrarTabla.html", "_blank");
-});
